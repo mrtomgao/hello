78bf3f2ed936cc0de39629e6228ee5d034804271
mrtomgao <34082794+sjhou@users.noreply.github.com>
2018-03-18 20:56:03 -0700

    lamer

--- js/toms.js
+++ js/toms.js
@@ -3,3 +3,24 @@
     Author: Tom Gao
     Description: This file contains the scripts associated with Tom's custom portfolio.
 */
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+            $('#back-to-top').fadeIn();
+        } else {
+            $('#back-to-top').fadeOut();
+        }
+    });
+    // scroll body to 0px on click
+    $('#back-to-top').click(function () {
+        $('#back-to-top').tooltip('hide');
+        $('body,html').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
+
+    $('#back-to-top').tooltip('show');
+
+});
