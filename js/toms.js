ac0654e754dc4b36de46a32b90412fdbedc1f383
mrtomgao <34082794+sjhou@users.noreply.github.com>
2018-03-19 23:41:41 -0700

    lamer

--- js/toms.js
+++ js/toms.js
@@ -3,24 +3,28 @@
     Author: Tom Gao
     Description: This file contains the scripts associated with Tom's custom portfolio.
 */
+// Get the modal
+var modal = document.getElementById('myModal');
 
-$(document).ready(function () {
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 50) {
-            $('#back-to-top').fadeIn();
-        } else {
-            $('#back-to-top').fadeOut();
-        }
-    });
-    // scroll body to 0px on click
-    $('#back-to-top').click(function () {
-        $('#back-to-top').tooltip('hide');
-        $('body,html').animate({
-            scrollTop: 0
-        }, 800);
-        return false;
-    });
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
 
-    $('#back-to-top').tooltip('show');
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-});
+// When the user clicks on the button, open the modal 
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
