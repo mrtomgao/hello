d59f806e7fd7e868f067f5b9467c30f4ee9eebb1
mrtomgao <34082794+sjhou@users.noreply.github.com>
2018-03-23 20:26:57 -0700

    new additions to projects

--- js/toms.js
+++ js/toms.js
@@ -3,28 +3,24 @@
     Author: Tom Gao
     Description: This file contains the scripts associated with Tom's custom portfolio.
 */
-// Get the modal
-var modal = document.getElementById('myModal');
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+$(document).ready(function () {
+    $(".card").click(function(){  
+        $("#modalProject").show();
+        $("#iframe1").attr("src", $(this).attr("href"));
+        return false;
+    });
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+    $("#myBtn").click(function () {
+        $("#modalPicture").show();
+        return false;
+    });
 
-// When the user clicks on the button, open the modal 
-btn.onclick = function () {
-    modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-    modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    window.onclick = function (event) {
+        $("#modalPicture").hide();
+        $("#modalProject").hide();
+        $("#iframe1").attr("src", "");
     }
-}
+});
+
+
